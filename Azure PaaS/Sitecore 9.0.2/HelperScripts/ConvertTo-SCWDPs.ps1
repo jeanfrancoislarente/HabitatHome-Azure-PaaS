b33d6ef13e87df66cc6993994658e618630a6555
auzunov-hhog <auzunov@hhogdev.com>
2018-09-25 20:05:24 +0300

    Adding parameters to the WDP module conversion script

--- Azure PaaS/Sitecore 9.0.2/HelperScripts/ConvertTo-SCWDPs.ps1
+++ Azure PaaS/Sitecore 9.0.2/HelperScripts/ConvertTo-SCWDPs.ps1
@@ -1,8 +1,16 @@
-Import-Module "C:\Users\auzunov\Downloads\ARM_deploy\1_Sitecore Azure Toolkit\tools\Sitecore.Cloud.Cmdlets.psm1" -Verbose
-Start-SitecoreAzureModulePackaging -SourceFolderPath "C:\_deployment\website_packaged" `
-                                   -DestinationFolderPath "C:\_deployment\website_packaged\convert to WDP\WPD" `
-                                   -CargoPayloadFolderPath "C:\_deployment\website_packaged\convert to WDP\Components\CargoPayloads" `
-                                   -AdditionalWdpContentsFolderPath "C:\_deployment\website_packaged\convert to WDP\Components\AdditionalFiles" `
-                                   -ParameterXmlFolderPath "C:\_deployment\website_packaged\convert to WDP\Components\MsDeployXmls" `
-                                   -ConfigFilePath "C:\_deployment\website_packaged\convert to WDP\Components\Configs\website.config.json" `
+$SitecoreCloudModulePath = "C:\Users\auzunov\Downloads\ARM_deploy\1_Sitecore Azure Toolkit\tools\Sitecore.Cloud.Cmdlets.psm1"
+$SourceFolderPath = "C:\_deployment\website_packaged"
+$DestinationFolderPath = "C:\_deployment\website_packaged\convert to WDP\WPD"
+$CargoPayloadFolderPath = "C:\_deployment\website_packaged\convert to WDP\Components\CargoPayloads"
+$AdditionalWdpContentsFolderPath = "C:\_deployment\website_packaged\convert to WDP\Components\AdditionalFiles"
+$ParameterXmlFolderPath = "C:\_deployment\website_packaged\convert to WDP\Components\MsDeployXmls"
+$ConfigFilePath = "C:\_deployment\website_packaged\convert to WDP\Components\Configs\website.config.json"
+
+Import-Module $SitecoreCloudModulePath -Verbose
+Start-SitecoreAzureModulePackaging -SourceFolderPath $SourceFolderPath `
+                                   -DestinationFolderPath $DestinationFolderPath `
+                                   -CargoPayloadFolderPath $CargoPayloadFolderPath `
+                                   -AdditionalWdpContentsFolderPath $AdditionalWdpContentsFolderPath `
+                                   -ParameterXmlFolderPath $ParameterXmlFolderPath `
+                                   -ConfigFilePath $ConfigFilePath `
                                    -Verbose
