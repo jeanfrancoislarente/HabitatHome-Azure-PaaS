a3f27c4c7d98e5ab29122b3d6ffead28d8e732d2
Keith Leon <KeithILeon@gmail.com>
2018-09-25 13:00:29 -0400

    change utilities folder name

--- Azure PaaS/Sitecore 9.0.2/HelperScripts/Upload-Packages.ps1
+++ Azure PaaS/Sitecore 9.0.2/HelperScripts/Upload-Packages.ps1
@@ -51,3 +51,26 @@ catch {
     Get-ChildItem -Path "C:\_deployment\website_packaged\convert to WDP\WPD\" -Recurse | Set-AzureStorageBlobContent -Container $additionalContainerName -Context $ctx
     
 }
+
+# Construct azuredeploy.parameters.json
+
+$cake_config = Get-Content 'C:\Users\auzunov\Source\Repos\HabitatHome-Azure-PaaS\Azure PaaS\Cake\cake-config.json' -raw | ConvertFrom-Json
+$deployemtId = $cake_config.AzureDeploymentID
+$location = $cake_config.AzureGeoLocation
+$sitecoreAdminPassword = $cake_config.SitecoreLoginAdminPassword
+$licenseXml = $cake_config.SitecoreLicenseXMLPath
+$sqlServerLogin = $cake_config.SqlServerLoginAdminAccount
+$sqlServerPassword = $cake_config.SqlServerLoginAdminPassword
+$authCertificatePassword = $cake_config.XConnectCertificatePassword
+
+$azuredeploy_template = Get-Content 'C:\Users\auzunov\Downloads\ARM_deploy\!Deployment\Azure PaaS\Sitecore 9.0.2\Xp Single\azuredeploy.parameters.json' -raw | ConvertFrom-Json
+$azuredeploy_template.parameters | % {
+    $_.deploymentId.value = $deployemtId
+    $_.location.value = $location
+    $_.sitecoreAdminPassword.value = $sitecoreAdminPassword
+    $_.licenseXml.value = $licenseXml
+    $_.sqlServerLogin.value = $sqlServerLogin
+    $_.sqlServerPassword.value = $sqlServerPassword
+    $_.authCertificatePassword.value = $authCertificatePassword
+}
+$azuredeploy_template | ConvertTo-Json -Depth 20 | Set-Content 'C:\Users\auzunov\Downloads\ARM_deploy\!Deployment\Azure PaaS\Sitecore 9.0.2\Xp Single\azuredeploy.parameters - Copy.json'
